28a8a7597eec22ca7201cc608ba02fedf23cc5bc
GerasimosTzivras <gerasimostzivras@outlook.com>
2024-06-09 20:24:38 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { TodoProvider } from "./TodoContext";
 import { Provider } from "react-redux";
 import store from "./store";
 
